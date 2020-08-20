d4eede087897dd1e264da2f3b2856437871383d5
saliha <salihagokce91@gmail.com>
2020-08-20 15:38:05 +0300

    first commit

---
+++ MachineLearningApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MachineLearningAppML.Model;
+namespace MachineLearningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+            input.SentimentText = "That is rude.";
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text:{input.SentimentText}\nIs Toxic:{result.Prediction}");
+            var inp = new ModelInput();
+            inp.SentimentText = "this is kind.";
+            ModelOutput res = ConsumeModel.Predict(inp);
+            Console.WriteLine($"text:{inp.SentimentText}\nIs Toxic:{res.Prediction}");
+        }
+    }
+}
